422d4806f87a7eb8330cbac03b1b13aac84760f8
An071003 <pekkidjj123@gmail.com>
2025-05-29 15:55:46 +0700

    refactor: Remove console log for API URL

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
   withCredentials: true,
