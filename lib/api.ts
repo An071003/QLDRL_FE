e10dcc4ee45f2ecf5efd958dcbdabeeaa949717e
An071003 <pekkidjj123@gmail.com>
2025-05-30 00:20:26 +0700

    fix(api): update baseURL to use NODE_ENV for production and local environments feat(next.config): add API rewrites for external endpoint

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
+  baseURL: process.env.NODE_ENV === 'production' ? '' : "http://localhost:5000",
   withCredentials: true,
 });
 
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     serverActions: {},
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://qldrlbe-e8grfucshahbe2d4.eastasia-01.azurewebsites.net/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
